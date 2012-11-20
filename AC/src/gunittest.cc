c9c9ad710c841bf549b8ce08de0eef7f6248b7f3
Jerry Yang <hy0kle@gmail.com>
2012-11-20 14:22:04 +0800

    test case for parse_args().

--- AC/src/gunittest.cc
+++ AC/src/gunittest.cc
@@ -4,7 +4,15 @@
 #include "util.h"
 #include "gtest/gtest.h"
 
-TEST(daemonize, Zero)
+TEST(daemonize, setDaemon)
 {
     EXPECT_EQ(0, daemonize(1, 1));
 }
+
+TEST(parse_args, parseArgs)
+{
+    int argc = 1;
+    char *argv[32] = {{"./test"}};
+    EXPECT_EQ(0, parse_args(argc, argv));
+}
+
