b020110e86d50402ee5a317001beddaa502f6181
Jerry Yang <hy0kle@gmail.com>
2017-11-02 09:57:16 +0800

    数学大法好呀!!!

--- php/monkey.php
+++ php/monkey.php
@@ -1,6 +1,7 @@
 <?php
 /**
- * @describe: 经典猴子出圈游戏
+ * @describe: 经典猴子出圈游戏 约瑟夫环
+ * @see: https://zh.wikipedia.org/wiki/%E7%BA%A6%E7%91%9F%E5%A4%AB%E6%96%AF%E9%97%AE%E9%A2%98
  * @author: Jerry Yang(hy0kle@gmail.com)
  * */
 function out_of_game($total, $cond)
@@ -31,5 +32,17 @@ function out_of_game($total, $cond)
 }
 
 echo 'The monkey King is: ', out_of_game(10, 3), PHP_EOL;
+
+// 数学法
+// 编号从0开始,如果编号要从1开始,
+function josephus($n, $k) {
+    $s = 0;
+    for ($i = 2; $i <= $n; $i++) {
+        $s = ($s + $k) % $i;
+    }
+
+    return $s;
+}
+echo 'The monkey King is(begin with 0): ', josephus(10, 3), PHP_EOL;
 /* vim:set ts=4 sw=4 et fdm=marker: */
 
