06823f8f048be46897d568460e5b193659186c72
Jerry Yang <hy0kle@gmail.com>
2018-01-09 20:01:27 +0800

    另一种随机红包算法,效率比环形算法更好

--- php/send-red-pack.php
+++ php/send-red-pack.php
@@ -34,6 +34,33 @@ function generate_conf($money, $number) {
     return $conf;
 }
 
-print_r(generate_conf(100, 3));
+$start_time = microtime(true);
+print_r(generate_conf(50000, 7));
+$end_time = microtime(true);
+echo 'generate_conf() spend time: ', ($end_time - $start_time) , PHP_EOL;
+
+function red_pack($money, $number) {
+    $conf = array();
+    for ($i = 0; $i < $number; $i++) {
+        $conf[] = 1;
+    }
+
+    $money -= $number;
+
+    while ($money > 0) {
+        $rand_money = mt_rand(1, $money);
+        $rand_key   = mt_rand(0, $number - 1);
+
+        $conf[$rand_key] += $rand_money;
+        $money -= $rand_money;
+    }
+
+    return $conf;
+}
+
+$start_time = microtime(true);
+print_r(red_pack(50000, 7));
+$end_time = microtime(true);
+echo 'red_pack() spend time: ', ($end_time - $start_time) , PHP_EOL;
 /* vim:set ts=4 sw=4 et fdm=marker: */
 
